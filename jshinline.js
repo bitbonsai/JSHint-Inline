39eed70a376c0862c9fd5b5e6302f2a33add5e2c
Mauricio Wolff <m@bitbonsai.com>
2013-08-14 04:04:35 -0300

    Removed local dependencies, JSHint is now global and used regex instead of cheerio, added package.json

--- jshinline.js
+++ jshinline.js
@@ -21,27 +21,42 @@
  *    distribution.
  */
 
-/*global console, process, require */
+/* globals console, process, require, __dirname */
 
 var fs = require('fs'),
     jshint = require('jshint').JSHINT,
+
+    buffer = '',
     buildpath = (__dirname.indexOf('\\') > -1) ? __dirname + '\\' : __dirname + '/',
+    code,
+    curfile = process.argv[2],
+    curfilename,
+    html,
+    jsh_counter = 0,
+    jsh_errors = [],
+    line;
+    match,
     options = JSON.parse(fs.readFileSync(buildpath + 'options.js', null).toString()),
-    curfile = process.argv[2];
+    re = /<script\b[^>]*>([\s\S]*?)<\/script>/gm,
+    script_lines = [],
+    script_tags = 0,
+    source = '',
+
+var errorLog = function (msg) {
+    'use strict';
+
+    console.log('✗ ' + msg);
+};
 
 if (!curfile) {
     errorLog('File needs to be saved before you can use JSHint-Inline.');
     return;
 }
 
-var curfilename = (curfile.indexOf('/') > -1) ? curfile.split('/') : curfile.split('\\');
-    curfilename = curfilename[curfilename.length - 1];
+curfilename = (curfile.indexOf('/') > -1) ? curfile.split('/') : curfile.split('\\');
+curfilename = curfilename[curfilename.length - 1];
 
-var html = fs.readFileSync(curfile, null).toString() || false;
-
-var errorLog = function (msg) {
-    console.log('✗ ' + msg);
-};
+html = fs.readFileSync(curfile, null).toString() || false;
 
 if (html) {
     html = html.toString();
@@ -49,86 +64,60 @@ if (html) {
     errorLog("Couldn't get any content from this file... File: " + curfile);
     return;
 }
-global =1
-var cheerio = require('cheerio');
-var $ = cheerio.load(html);
-
-var $script_tags = $('script');
-
-if (!$script_tags.length) {
-    errorLog("This file doesn't have any <script> tags... Are you sure you're in the right tab? This one is: " + curfile);
-    return;
-}
-
-var script_lines = [];
 
 html.split('\n').forEach(function (str, i) {
+    'use strict';
+
     if (str.indexOf('<script') > -1) {
         script_lines.push(i);
     }
 });
 
-var jsh_counter = 0;
-var jsh_errors = 0;
+buffer += '[JSHint Inline: ' + curfile + ']\n\n';
 
-$script_tags.each(function () {
-    var source = this.html();
+while (match = re.exec(html)){
+    script_tags++;
+    source = match[1];
 
     try {
         jshint(source, options);
     } catch(e) {}
 
     if (jshint.errors) {
-
-        function numberWang(wangaNumb) {
-            var
-            thatsNumberWang = 5 - wangaNumb,
-                stayNumberWang = '',
-                i;
-
-            for (i = 0; i < thatsNumberWang; i += 1) {
-                stayNumberWang += ' ';
-            }
-
-            return stayNumberWang;
-        }
-
-        jsh_errors += jshint.errors.length;
-
         jshint.errors.forEach(function(e) {
+            'use strict';
+
             // if the error is null, then we could not continue (too many errors)
             if (e === null) {
-              errorLog("Stopping, unable to continue.");
+              errorLog('Stopping, unable to continue. Too many errors.');
               return;
             }
 
-            // get the raw error data
-            var raw = e.raw;
-
             // do some formatting if the error data is available
-            if ("undefined" !== typeof raw) {
-                var code = ' (' + e.code + ')';
-                var line = e.line + script_lines[jsh_counter];
-                errorLog(
-                [
-                    curfilename, " - ",
-                    line, ":",
-                    e.character, " ",
-                    raw.replace("{a}", e.a).
-                    replace("{b}", e.b).
-                    replace("{c}", e.c).
-                    replace("{d}", e.d),
-                    code
-                ].join(""));
+            if ("undefined" !== typeof e.raw) {
+                code = ' (' + e.code + ')';
+                line = e.line + script_lines[jsh_counter] - 1;
+                
+                jsh_errors.push('  ' + line + ',' + e.character + ': ' + e.reason + code);
             }
         });
     }
     jsh_counter++;
-});
-
-if (jsh_errors === 0) {
-    console.log('✓ JSHint PASSED');
-} else {
-    var plural = (jsh_errors > 1) ? 's' : '';
-    console.log('✗ JSHint FAILED (' + jsh_errors + ' error' + plural + ' found)');
 }
+
+if (script_tags === 0) {
+    errorLog('Stopping, unable to continue. This html file does not have any <script> tag.');
+    return;
+}
+
+if (!jsh_errors.length) {
+    buffer += '✓ JSHint PASSED! 0 errors, [esc] to hide.\n';
+} else {
+    var plural = (jsh_counter > 1) ? 's' : '';
+    buffer += jsh_errors.join('\n');
+    buffer += '\n\n';
+    buffer += '✗ JSHint FAILED\n  ' + jsh_counter + ' warning'+ plural +'/error' + plural + ' detected.\n';
+    buffer +=  '\n';
+}
+
+console.log(buffer);
